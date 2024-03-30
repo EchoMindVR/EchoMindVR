90f712366d3b309a94b429111bd134ce00e80508
Ruihang Zhang <ruihang.zhang@mail.utoronto.ca>
2024-03-30 16:57:17 -0400

    WIP

--- backend/llm/pdf_split.py
+++ backend/llm/pdf_split.py
@@ -8,23 +8,24 @@ from langchain_community.embeddings.sentence_transformer import (
 import pickle
 import os
 
-first_flag = True
-# Load and split the pages of all PDF documents in documents directory
-base_dir = os.path.dirname(__file__)
-documents_folder = os.path.join(base_dir, 'documents')
-for file in os.listdir(documents_folder):
-    if first_flag:
-        merge_loader = PyPDFLoader(os.path.join(documents_folder, file))
-        first_flag = False
-    if file.endswith(".pdf"):
-        loader = PyPDFLoader(os.path.join(documents_folder, file))
-        merge_loader = MergedDataLoader(loaders=[merge_loader, loader])
+def load_pdfs_into_chroma_db():
+    first_flag = True
+    # Load and split the pages of all PDF documents in documents directory
+    base_dir = os.path.dirname(__file__)
+    documents_folder = os.path.join(base_dir, 'documents')
+    for file in os.listdir(documents_folder):
+        if first_flag:
+            merge_loader = PyPDFLoader(os.path.join(documents_folder, file))
+            first_flag = False
+        if file.endswith(".pdf"):
+            loader = PyPDFLoader(os.path.join(documents_folder, file))
+            merge_loader = MergedDataLoader(loaders=[merge_loader, loader])
 
-pages = merge_loader.load()
+    pages = merge_loader.load()
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
-pages = text_splitter.split_documents(pages)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+    pages = text_splitter.split_documents(pages)
 
-embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-chroma_dir = os.path.join(base_dir, 'chroma_db')
-vectorstore = Chroma.from_documents(pages, embedding_function, persist_directory=chroma_dir)
+    embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+    chroma_dir = os.path.join(base_dir, 'chroma_db')
+    vectorstore = Chroma.from_documents(pages, embedding_function, persist_directory=chroma_dir)
