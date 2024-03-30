67a264a6472ec47a40da66742084ca565844e091
bennywu2019 <benny.wu.new@gmail.com>
2024-03-30 13:50:00 -0400

    wawawa

--- vision-pro/vision-pro/vision_proApp.swift
+++ vision-pro/vision-pro/vision_proApp.swift
@@ -10,12 +10,14 @@ import SwiftUI
 @main
 struct Vision_Pro: App {
     var body: some Scene {
-        WindowGroup {
+        WindowGroup("Learning App", id: "modules") {
             ContentView()
         }
-
+        .defaultSize(CGSize(width: 1500, height: 850))
+    
         ImmersiveSpace(id: "ImmersiveSpace") {
             ImmersiveView()
         }.immersionStyle(selection: .constant(.full), in: .full)
+        
     }
 }
